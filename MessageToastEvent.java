6bbb8aa77fc897075351b07df6a4776447ae7086
selimolmaz <46197354+selimolmaz@users.noreply.github.com>
2022-04-25 00:32:06 +0300

    Add files via upload

---
+++ MessageToastEvent.java
@@ -0,0 +1,9 @@
+package com.example.eventbusdouble;
+
+public class MessageToastEvent {
+    public final String message;
+
+    public MessageToastEvent(String message) {
+        this.message = message;
+    }
+}
